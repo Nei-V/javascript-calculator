c2c50a3d500e3ac1715947e6c051c270e900fd97
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-05-16 01:09:39 +0300

    better css and javascript
    Signed-off-by: Ion Varsescu <19718355+Nei-V@users.noreply.github.com>

--- javascript calculator/javascriptcalculator.js
+++ javascript calculator/javascriptcalculator.js
@@ -1,5 +1,24 @@
 function test() {
+    let fullExercise = document.getElementById("show-exercise").children[0].innerHTML;
+    console.log(fullExercise);
+
+
+    let result = 0;
+    // document.getElementById("show-result").innerHTML=0;
     document.getElementById("testing").innerHTML = "TEST OK";
+};
+
+function typeExercise() {
+    let newExercise = "1";
+    if (document.getElementById("show-exercise").children[0].innerHTML==0){
+        document.getElementById("show-exercise").children[0].innerHTML = newExercise;   
+    }
+    else if (document.getElementById("show-exercise").children[0].innerHTML.length < 33) {
+        document.getElementById("show-exercise").children[0].innerHTML += newExercise;
+    }
+    else {
+        alert("not enough space")
+    };
 };
 
 test();
--- javascript calculator/lessons learned.md
+++ javascript calculator/lessons learned.md
@@ -1,1 +1,14 @@
 1. Semantic elements in HTML5
+
+2. CSS grid +  naming areas
+
+3. CSS selectors
+
+4. CSS shadows
+More shadows can be added separated by commas, 
+using "inset" for shadows in the object
+like in button:hover
+
+5. CSS:hover + shadows
+
+6. CSS text-align has to be used with width 100% and without display:inline
