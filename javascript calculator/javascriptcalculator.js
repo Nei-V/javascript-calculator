51afabe9632b4d1c99ce32d42e5f6b833fb94f11
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-05-18 01:45:41 +0300

    different approach in js - different actions, checks for dot and 0, AC

--- javascript calculator/javascriptcalculator.js
+++ javascript calculator/javascriptcalculator.js
@@ -8,51 +8,140 @@ function test() {
     document.getElementById("testing").innerHTML = "TEST OK";
 };
 
+test();
+
+let signUsed = false;
+let isSign = /\+|\-|\/|\*/;
+let isDigitNotZero = /[1-9]/;
+let toBeShown = /\d|\.|\+|\-|\/|\*/; /*regex for digit or dot or signs*/
+let noOtherDot = /\.+/;
+
+
+
+
+
+
 function typeExercise(pressed) {
     let newExercise = pressed;
-    if (document.getElementById("show-exercise").children[0].innerHTML == 0) {
+    if (document.getElementById("show-exercise").children[0].innerHTML == "0") {
         document.getElementById("show-exercise").children[0].innerHTML = newExercise;
     }
     else if (document.getElementById("show-exercise").children[0].innerHTML.length < 31) {
         document.getElementById("show-exercise").children[0].innerHTML += newExercise;
     }
     else {
-        alert("not enough space")
+        alert("not enough space");
     };
 };
-
-/*adding click event to digits and dot or signs - for shown line*/
-document.body.addEventListener("click", event => {
-    let toBeShown = /\d|\.|\+|\-|\/|\*/ /*regex for digit or dot or signs*/
-console.log(toBeShown);
-if ((event.target.nodeName == "BUTTON") && (toBeShown.test(event.target.textContent))) {
-    console.log("Clicked", event.target.textContent);
-    typeExercise(event.target.textContent);
-}
-});
 
 function typeNumbers(pressed) {
     let newExercise = pressed;
-    if (document.getElementById("show-result").children[0].innerHTML == 0) {
+    if ((document.getElementById("show-result").children[0].innerHTML == "0") || (signUsed)) {
         document.getElementById("show-result").children[0].innerHTML = newExercise;
+        typeExercise(pressed);
     }
     else if (document.getElementById("show-result").children[0].innerHTML.length < 20) {
         document.getElementById("show-result").children[0].innerHTML += newExercise;
+        typeExercise(pressed);
     }
     else {
-        alert("not enough space")
+        alert("not enough space");
     };
 };
 
-/*adding click event to digits and dot for result screen*/
+
 document.body.addEventListener("click", event => {
-    let toBeShown = /\d|\./ /*regex for digit or dot */
+    if ((event.target.nodeName == "BUTTON") && (event.target.textContent == "AC")) {
+        console.log("Clicked", event.target.textContent);
+        document.getElementById("show-result").children[0].innerHTML = 0;
+        document.getElementById("show-exercise").children[0].innerHTML = 0;
+        signUsed = false;
+    };
+    if ((event.target.nodeName == "BUTTON") && (isDigitNotZero.test(event.target.textContent))) {
+        console.log("Clicked", event.target.textContent);
+        //typeExercise(event.target.textContent);
+        typeNumbers(event.target.textContent);
+        signUsed = false;
+    };
+    if ((event.target.nodeName == "BUTTON") && (event.target.textContent == "0")) {
+        console.log("Clicked", event.target.textContent);
+        //typeExercise(event.target.textContent);
+        typeNumbers(event.target.textContent);
+        signUsed = false;
+    };
+    if ((event.target.nodeName == "BUTTON") && (event.target.textContent == ".")) {
+        console.log("Clicked", event.target.textContent);
+        if (signUsed == false) {
+            if (document.getElementById("show-result").children[0].innerHTML.length < 20) {
+                
+                if (noOtherDot.test(document.getElementById("show-result").children[0].innerHTML) == false) {
+                    document.getElementById("show-result").children[0].innerHTML += ".";
+                    document.getElementById("show-exercise").children[0].innerHTML += ".";
+                };
+            }
+            else {
+                alert("not enough space");
+            };
+        };
+
+
+        //typeNumbers(event.target.textContent);
+        signUsed = false;
+    };
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+/*adding click event to digits and dot or signs - for shown line
+document.body.addEventListener("click", event => {
+    console.log(signUsed);
+
+
+    console.log(toBeShown);
+    if ((event.target.nodeName == "BUTTON") && (toBeShown.test(event.target.textContent))) {
+        console.log("Clicked", event.target.textContent);
+        if isSign.test(event.target.textContent){
+            signUsed = true;
+        };
+        if (signUsed == false) {
+            typeExercise(event.target.textContent);
+        }
+        else {
+
+        }
+        
+
+    };
+
+});
+
+
+
+adding click event to digits and dot for result screen
+document.body.addEventListener("click", event => {
+    let toBeShown = /\d|\./ ----->regex for digit or dot 
     console.log(toBeShown);
     if ((event.target.nodeName == "BUTTON") && (toBeShown.test(event.target.textContent))) {
         console.log("Clicked", event.target.textContent);
         typeNumbers(event.target.textContent);
-    }
+    };
 });
 
 
-test();
+*/
